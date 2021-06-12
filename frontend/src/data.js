f6b24f7813768a0ad6458fa0f520b4a80f67f507
mariglen <geni.kurti1994@gmail.com>
2021-06-12 23:01:24 +0200

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "jeance nike",
+      categorie: "jeance",
+      image: "/images/p1.webp",
+      price: 120,
+      brand: "nike",
+      rating: 4.5,
+      nrReviews: 10,
+      description: "nike jeance",
+    },
+    {
+      _id: "2",
+      name: "jeance adidas",
+      categorie: "jeance",
+      image: "/images/p2.webp",
+      price: 100,
+      brand: "adidas",
+      rating: 4.5,
+      nrReviews: 10,
+      description: "nike jeance",
+    },
+    {
+      _id: "3",
+      name: "jeance puma",
+      categorie: "jeance",
+      image: "/images/p3.webp",
+      price: 130,
+      brand: "puma",
+      rating: 4.5,
+      nrReviews: 10,
+      description: "nike jeance",
+    },
+    {
+      _id: "4",
+      name: "shirt nike",
+      categorie: "shirt",
+      image: "/images/p4.webp",
+      price: 80,
+      brand: "nike",
+      rating: 4.5,
+      nrReviews: 10,
+      description: "nike jeance",
+    },
+    {
+      _id: "5",
+      name: "shirt adidas",
+      categorie: "shirt",
+      image: "/images/p5.webp",
+      price: 50,
+      brand: "nike",
+      rating: 4.5,
+      nrReviews: 10,
+      description: "nike jeance",
+    },
+  ],
+};
+
+export default data;
